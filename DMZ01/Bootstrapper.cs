4366e677e02f6ebdb344ef05389256784b7afd5e
Hogan2 <loggg2333@gmail.com>
2018-04-20 17:29:18 +0800

    00

---
+++ DMZ01/Bootstrapper.cs
@@ -0,0 +1,19 @@
+﻿using DMZ01.Views;
+using Microsoft.Practices.Unity;
+using Prism.Unity;
+using System.Windows;
+
+namespace DMZ01
+{
+    class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+    }
+}
--- DMZ01/Views/MainWindow.xaml.cs
+++ DMZ01/Views/MainWindow.xaml.cs
@@ -10,10 +10,9 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace DMZ01
+namespace DMZ01.Views
 {
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
